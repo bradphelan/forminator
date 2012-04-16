4a864416a42b31d3c44ec9dd8d1a37f341b85f4f
Brad Phelan <bradphelan@xtargets.com>
2012-04-16 17:12:36 +0200

    Fix cache

--- forminator.rb
+++ forminator.rb
@@ -3,7 +3,7 @@ use Rack::Auth::Basic, "Restricted Area" do |username, password|
   [username, password] == ['admin', 'nimda']
 end
 
-set :static_cache_control, :public, :max_age => 1
+set :static_cache_control, [:public, :max_age => 0]
 
 case ENV["FORMINATOR_ENV"]
 when "production"
